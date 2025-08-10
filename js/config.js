0dd8018942a6822d0e1f9788bfe74026cdc1474a
Thebuckybot <berichtenboxtom@gmail.com>
2025-08-11 00:53:36 +0200

    back slash

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // config.js
 const API_URL = "https://diego-directions-pace-key.trycloudflare.com";
-const FRONTEND_URL = "https://thebuckybot.github.io/site/";
+const FRONTEND_URL = "https://thebuckybot.github.io/site";
 
 export { API_URL, FRONTEND_URL };
