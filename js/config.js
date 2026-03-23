0fbd6897d1a0bff72483703ffd2b1038865ae76e
Thebuckybot <berichtenboxtom@gmail.com>
2026-03-23 13:52:55 +0100

    test met andere url...

--- js/config.js
+++ js/config.js
@@ -1,4 +1,6 @@
 // config.js
-const API_URL = "https://unavailable-cleaner-starts-pvc.trycloudflare.com";
+// const API_URL = "https://unavailable-cleaner-starts-pvc.trycloudflare.com";
+
+const API_URL = window.location.origin;
 
 export { API_URL };
