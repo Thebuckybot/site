4be52e4ae0dd8283a68802d219dd7e52eb928d60
Thebuckybot <berichtenboxtom@gmail.com>
2026-03-23 20:02:09 +0100

    change link

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // config.js
-const API_URL = "https://many-cherry-introduces-determines.trycloudflare.com";
+const API_URL = "https://jeans-scoop-mazda-mean.trycloudflare.com";
 
 export { API_URL };
