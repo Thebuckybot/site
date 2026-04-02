caf562014a2f87c3d8db32c53ff3a2b633977e6f
Bucky <berichtenboxtom@gmail.com>
2026-04-02 21:20:22 +0200

    update link

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // config.js
-const API_URL = "https://enhancement-lopez-savings-goal.trycloudflare.com";
+const API_URL = "https://recording-tsunami-month-entities.trycloudflare.com";
 
 export { API_URL };
