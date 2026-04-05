023733f1786f8dbaa33b9024cda9dbbe3741b35c
Bucky <berichtenboxtom@gmail.com>
2026-04-05 22:34:45 +0200

    update link

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // config.js
-const API_URL = "https://recording-tsunami-month-entities.trycloudflare.com";
+const API_URL = "https://messenger-stylish-winston-xml.trycloudflare.com";
 
 export { API_URL };
